41123adb9f29cbdb89d6853a82fa389b91c7f758
Pena Daniel (dev@dan) <penatsoptedanieljosue@gmail.com>
2024-09-18 15:45:22 +0200

    add constructor on the fraction method

--- src/main/java/com/tp1/Fraction.java
+++ src/main/java/com/tp1/Fraction.java
@@ -1,8 +1,31 @@
 package com.tp1;
 
 public class Fraction {
+    private Integer nominateur;
+    private Integer denominateur;
+
+    public Fraction(Integer nominateur, Integer denominateur) {
+        this.nominateur = nominateur;
+        if (denominateur == 0){
+            this.denominateur = 1;
+        }
+        this.denominateur = denominateur;
+    }
+
+    public Fraction(Integer nominateur) {
+        this.nominateur = nominateur;
+        this.denominateur = 1;
+    }
+
+    public Fraction() {
+        this.nominateur = 0;
+        this.denominateur = 1;
+    }
+
     @Override
     public String toString() {
-        return "Je suis une fraction";
+        return "Je suis une fraction de nominateur "+nominateur+" et de denominateur "+denominateur;
     }
+
+
 }
--- src/main/java/com/tp1/Main.java
+++ src/main/java/com/tp1/Main.java
@@ -3,8 +3,16 @@ package com.tp1;
 public class Main {
     public static void main(String[] args) {
         Fraction f = new Fraction();
+        Fraction f1 = new Fraction(1);
+        Fraction f2 = new Fraction(1, 2);
 
+        // Question 1 tester les constructeurs
+        assert f.toString().equals("Je suis une fraction de nominateur 0 et de denominateur 1");
+        assert f1.toString().equals("Je suis une fraction de nominateur 1 et de denominateur 1");
+        assert f2.toString().equals("Je suis une fraction de nominateur 1 et de denominateur 2");
         System.out.println(f);
+        System.out.println(f1);
+        System.out.println(f2);
 
     }
 }
